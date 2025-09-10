28c3ef983fb827c506d2f1aa350849cc4de82bde
Hatsune Miku <l2a1knla@gmail.com>
2025-09-10 17:19:05 +0800

    first

--- src/App.jsx
+++ src/App.jsx
@@ -140,7 +140,7 @@ function App() {
 export default App
 
 function encode(s) {
-  return AES.encrypt(s, '111sigewinne').toString(enc.Utf8)
+  return AES.encrypt(s, '111sigewinne').toString()
 }
 
 function decode(s) {
