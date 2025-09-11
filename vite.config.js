dc5b71e03fda7f6dea585a300242ef468f4eec89
Hatsune Miku <l2a1knla@gmail.com>
2025-09-11 17:01:48 +0800

    test

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,29 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react-swc'
+import obfuscatorPlugin from 'vite-plugin-javascript-obfuscator'
 
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: [react()],
+  plugins: [
+    react(),
+    obfuscatorPlugin({
+      include: ['src/**/*.js', 'src/**/*.jsx'],
+      exclude: [/node_modules/],
+      options: {
+        controlFlowFlattening: true,
+        controlFlowFlatteningThreshold: 1,
+        numbersToExpressions: true,
+        debugProtection: true,
+        stringArray: true,
+        stringArrayShuffle: true,
+        stringArrayEncoding: ['rc4'],
+        selfDefending: true,
+        renameGlobals: true,
+        identifiersPrefix: '_vc',
+        identifierNamesGenerator: 'hexadecimal',
+        deadCodeInjection: true,
+        deadCodeInjectionThreshold: 0.4,
+      },
+    }),
+  ],
 })
